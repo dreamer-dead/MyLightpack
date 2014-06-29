cfd75742169585146b19d3eb8761c5813a35e55e
dreamer-dead <dreamer.dead@gmail.com>
2014-06-29 20:45:49 +0400

    Additional Settings fixes

--- Software/src/Settings.hpp
+++ Software/src/Settings.hpp
@@ -201,6 +201,7 @@ public:
     // [Adalight | Ardulight | Lightpack | ... | Virtual]
 	void setNumberOfLeds(SupportedDevices::DeviceType device, int numberOfLeds);
 	int getNumberOfLeds(SupportedDevices::DeviceType device) const;
+	int getNumberOfConnectedDeviceLeds() const;
 
 	void setColorSequence(SupportedDevices::DeviceType device, QString colorSequence);
 	QString getColorSequence(SupportedDevices::DeviceType device) const;
@@ -230,7 +231,7 @@ public:
 	double getDeviceGamma() const;
 	void setDeviceGamma(double gamma);
 
-	Grab::GrabberType getGrabberType();
+	Grab::GrabberType getGrabberType() const;
 	void setGrabberType(Grab::GrabberType grabMode);
 
 #ifdef D3D10_GRAB_SUPPORT
