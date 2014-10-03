woodenshark/Lightpack

Implement LightpackCommandLineParser class.
	- Use QCommandLineParser class to parse the command line.
	- Add alternate way to set debug level (--debug=level).
	- Handle --help and --version command switches.
	- Add unit-tests for LightpackCommandLineParser class.

Software/tests/TestsMain.cpp
@@ -7,6 +7,7 @@
 #ifdef Q_OS_WIN
 #include "HooksTest.h"
 #endif
+#include "LightpackCommandLineParserTest.hpp"
 #include "debug.h"
 
 #include <iostream>
@@ -22,7 +23,6 @@ int main(int argc, char *argv[])
 
     QList<QObject *> tests;
     QStringList summary;
-
     tests.append(new GrabCalculationTest());
 
 #ifdef Q_OS_WIN
@@ -32,9 +32,7 @@ int main(int argc, char *argv[])
     tests.append(new LightpackMathTest());
     tests.append(new LightpackApiTest());
     tests.append(new AppVersionTest());
-
-
-
+    tests.append(new LightpackCommandLineParserTest());
 
     for(int i=0; i < tests.size(); i++) {
         if (QTest::qExec(tests[i], argc, argv)) {

dreamer-dead/MyLightpack

Software/tests/TestsMain.cpp
@@ -1,4 +1,3 @@
-
 #include <QtTest/QtTest>
 #include "LightpackApiTest.hpp"
 #include "GrabCalculationTest.hpp"
@@ -9,6 +8,7 @@
 #ifdef Q_OS_WIN
 #include "HooksTest.h"
 #endif
+#include "LightpackCommandLineParserTest.hpp"
 #include "debug.h"
 
 #include <iostream>
@@ -24,20 +24,17 @@ int main(int argc, char *argv[])
 
     QList<QObject *> tests;
     QStringList summary;
-
-#if 0
     tests.append(new GrabCalculationTest());
 
 #ifdef Q_OS_WIN
     tests.append(new HooksTest());
 #endif
 
-	tests.append(new LightpackMathTest());
-	tests.append(new LightpackApiTest());
-	tests.append(new AppVersionTest());
-#endif
-	tests.append(new SettingsSourceMockupTest());
-	tests.append(new SettingsTest());
+    tests.append(new LightpackMathTest());
+    tests.append(new LightpackApiTest());
+    tests.append(new AppVersionTest());
+    tests.append(new SettingsSourceMockupTest());
+    tests.append(new SettingsTest());
 
     for(int i=0; i < tests.size(); i++) {
         if (QTest::qExec(tests[i], argc, argv)) {