36f9edaf75342f46f755464b44506c485ff01d17
dreamer-dead <dreamer.dead@gmail.com>
2014-10-12 22:30:59 +0400

    Move SettingsSourceMockupTest to gtest.

--- Software/tests/SettingsSourceMockupTest.cpp
+++ Software/tests/SettingsSourceMockupTest.cpp
@@ -1,28 +1,24 @@
-#include "SettingsSourceMockupTest.hpp"
 #include "SettingsSourceMockup.hpp"
+#include "gtest/gtest.h"
 
-SettingsSourceMockupTest::SettingsSourceMockupTest(QObject *parent) :
-	QObject(parent) {
-}
-
-void SettingsSourceMockupTest::testCase_AddValue() {
+TEST(SettingsSourceMockupTest, AddValue) {
 	SettingsSourceMockup settingsSource;
 	const QString key("SomeKey");
 	const QVariant value(100500);
 
-	QVERIFY(!settingsSource.contains(key));
+	EXPECT_FALSE(settingsSource.contains(key));
 	settingsSource.setValue(key, value);
-	QVERIFY(settingsSource.contains(key));
-	QCOMPARE(settingsSource.value(key), value);
+	EXPECT_TRUE(settingsSource.contains(key));
+	EXPECT_EQ(settingsSource.value(key), value);
 }
 
-void SettingsSourceMockupTest::testCase_RemoveValue() {
+TEST(SettingsSourceMockupTest, RemoveValue) {
 	SettingsSourceMockup settingsSource;
 	const QString key("SomeKey");
 	const QVariant value(100500);
 
 	settingsSource.setValue(key, value);
-	QVERIFY(settingsSource.contains(key));
+	EXPECT_TRUE(settingsSource.contains(key));
 	settingsSource.remove(key);
-	QVERIFY(!settingsSource.contains(key));
+	EXPECT_FALSE(settingsSource.contains(key));
 }
