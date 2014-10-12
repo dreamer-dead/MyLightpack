0240da2d12b32118ab7b567c91334513f89d7c12
dreamer-dead <dreamer.dead@gmail.com>
2014-10-12 22:21:34 +0400

    Fix GrabCalculationTest and change it to gtest.

--- Software/tests/GrabCalculationTest.cpp
+++ Software/tests/GrabCalculationTest.cpp
@@ -1,9 +1,41 @@
-#include "GrabCalculationTest.hpp"
+/*
+ * tst_GrabCalculationTestTest.cpp
+ *
+ *  Project: Lightpack
+ *
+ *  Lightpack a USB content-driving ambient lighting system
+ *
+ *  Lightpack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lightpack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-void GrabCalculationTest::testCase1()
-{
+#include <QColor>
+#include <QRgb>
+#include <QRect>
+
+#include "enums.hpp"
+#include "calculations.hpp"
+#include "gtest/gtest.h"
+
+TEST(GrabCalculationTest, AvgColor) {
+    using Grab::Calculations::calculateAvgColor;
+
     QRgb result;
-    unsigned char buf[16];
-    memset(buf, 0xfa, 16);
-    QVERIFY2(Grab::Calculations::calculateAvgColor(&result, buf, BufferFormatArgb, 16, QRect(0,0,4,1)) == QColor(0xfa, 0xfa, 0xfa).rgb(), qPrintable(QString("Failure. calculateAvgColor returned wrong errorcode %1").arg(result, 1, 16)));
+    unsigned char buffer[16];
+    memset(buffer, 0xfa, sizeof(buffer));
+    const QRgb color = calculateAvgColor(
+        &result, buffer, BufferFormatArgb, sizeof(buffer), QRect(0, 0, 4, 1));
+    EXPECT_EQ(QColor(0xfa, 0xfa, 0xfa, 0xfa).rgb(), color)
+        << "Failure. calculateAvgColor returned wrong errorcode " << result;
 }
