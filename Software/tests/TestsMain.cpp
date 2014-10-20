1b6fc803cdc59e78df3b65089b4fe68f6f9cc916
dreamer-dead <dreamer.dead@gmail.com>
2014-10-20 15:57:02 +0400

    Run Qt message loop in tests.

--- Software/tests/TestsMain.cpp
+++ Software/tests/TestsMain.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <QApplication>
+#include <QTimer>
 
 #include "common/DebugOut.hpp"
 #include "gtest/gtest.h"
@@ -8,11 +9,31 @@ using namespace std;
 
 unsigned g_debugLevel = Debug::LowLevel;
 
+namespace {
+class TestApplication : public QApplication {
+    Q_OBJECT
+public:
+    TestApplication(int &argc, char **argv) : QApplication(argc, argv) {
+        testing::InitGoogleTest(&argc, argv);
+    }
+
+    int exec() const {
+        QTimer::singleShot(0, this, SLOT(onEventLoopStarted()));
+        return QApplication::exec();
+    }
+
+public Q_SLOTS:
+    void onEventLoopStarted() {
+        cout << "Run Lightpack tests:" << endl;
+        QCoreApplication::exit(RUN_ALL_TESTS());
+    }
+};
+}
+
+#include "TestsMain.moc"
+
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-
-    cout << "Run Lightpack tests:" << endl;
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    TestApplication app(argc, argv);
+    return app.exec();
 }
